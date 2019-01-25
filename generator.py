50f816dfd2f65215e0566a3cb57c157b510978e2
alexnurin <44980361+alexnurin@users.noreply.github.com>
2019-01-25 20:25:24 +0300

    Поддержка data в generator.py

--- generator.py
+++ generator.py
@@ -4,13 +4,12 @@ from params import *
 
 file_res = open('output.txt', 'w')
 lastG = '1'
-
-print(datetime.time(datetime.now()))
+print(datetime.time(datetime.now()), data)
 for i in range(n):
-    file = open('data/' + lastG + '.txt', 'r')
+    file = open(data + lastG + '.txt', 'r')
     textG_all = file.readlines()
     if len(textG_all) == 0:
-        file = open('data/1.txt', 'r')
+        file = open(data + '1.txt', 'r')
         textG_all = file.readlines()
     textG = textG_all[randint(1, len(textG_all)) - 1][:-1]
     file_res.write(" " + textG.replace('1', '.'))
