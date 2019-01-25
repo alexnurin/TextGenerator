d2e6a53833317a521f9df4df650cff8a829574ac
alexnurin <44980361+alexnurin@users.noreply.github.com>
2019-01-25 20:23:41 +0300

    Добавил data как переменную
    Прописал поддержку нескольких папок с данными (тексты разных жанров, например)

--- params.py
+++ params.py
@@ -1,5 +1,6 @@
 import re
 
-n = 40
-bigSet = {i for i in re.compile('[^a-zа-яA-ZА-Я:. \n]').sub('', open("names.txt", 'r', encoding='utf-8').read()).split()}
-newString = 15
+n = 50
+#bigSet = {i for i in re.compile('[^a-zа-яA-ZА-Я:. \n]').sub('', open("names.txt", 'r', encoding='utf-8').read()).split()}
+newString = 100
+data = "data/"
